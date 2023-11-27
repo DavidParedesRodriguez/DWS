05a7b874bb1f8646323ad1930f5026d72e44abe3
DavidParedesRodriguez <deividregalon2002@gmail.com>
2023-11-27 21:06:51 +0100

    proyectoHotel_hecho

---
+++ ProyectoHoteles/error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Error</title>
+</head>
+<body>
+    <div id="formulario-container">
+        <h2>Error al procesar el formulario</h2>
+        <p>Hubo un error al procesar el formulario. Por favor, inténtalo nuevamente.</p>
+      
+    </div>
+</body>
+</html>
---
+++ ProyectoHoteles/exito.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Éxito</title>
+</head>
+<body>
+    <div id="formulario-container">
+        <h2>Hotel añadido con éxito</h2>
+        <p>El hotel ha sido añadido correctamente.</p>
+       
+    </div>
+</body>
+</html>
